f12616817daf4c57c0241182c4d8fdb8c081088b
Yun-Hsuan Chou <Yun-Hsuan@localhost.localdomain>
2015-12-09 16:42:35 +0800

    ReverseInt

---
+++ ReverseInt/ReverseInt.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int reverse(int x){
+	char num_str[16];
+	int num = 0;	
+	sprintf(num_str, "%d", x);
+	for(size_t i = 0; i < strlen(num_str); i++)
+				
+}
+
+int main(){
+
+	return 0;
+}
